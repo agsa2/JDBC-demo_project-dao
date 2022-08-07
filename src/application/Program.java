365f060b9537ab3d938145603084acfe5cc04c65
Alvaro Gabriel <agsa0108@gmail.com>
2022-08-07 17:18:56 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,16 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=== TEST 3 seller findByAll ====");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		
+		
+		
 	}
 
 }
