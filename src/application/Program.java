11d139e97a9e67cf6b7a3e44ed1cb482778f99cb
Alvaro Gabriel <agsa0108@gmail.com>
2022-08-07 16:17:17 -0300

    Instantiaton functions

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = sellerDao.findById(1);
+		System.out.println("=== TEST 1 seller findById ====");
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
