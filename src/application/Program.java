a62ccdd6dd88b8601ab58959f405faae48211093
Alvaro Gabriel <agsa0108@gmail.com>
2022-08-08 18:13:59 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,11 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id: " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5 seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Alvaro Gabriel");
+		sellerDao.update(seller);
+		System.out.println("Update complete!");
 	}
 
 }
